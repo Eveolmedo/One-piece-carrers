3c222b0cb97be26e2652ac54d569c6d9610df6e3
Evelyn Olmedo <evelynolmeedo@gmail.com>
2023-06-25 16:36:51 -0300

    Add form to add new job in modal

--- js/main.js
+++ js/main.js
@@ -4,7 +4,7 @@ const $$ = (selector) => document.querySelectorAll(selector)
 const cleanContainer = (selector) => $(selector).innerHTML = ""
 
 const hideElement = (selector) => $(selector).style.display = "none"
-const showElement = (selector) => $(selector).style.display = "block"
+const showElement = (selector) => $(selector).style.display = "flex"
 
 const getJobs = () => {
     fetch(`https://6487a5a4beba62972790debd.mockapi.io/jobs`)
@@ -15,14 +15,34 @@ const getJobs = () => {
 getJobs()
 
 const renderJobs = (jobs) => {
-    for (const {image, name, category} of jobs) {
+    for (const {image, name, description} of jobs) {
         $("#job-container").innerHTML += `
             <div class="job-card">
                 <img class="job-image" src="${image}">
                 <h3>${name}</h3>
-                <p>${category}</p>
+                <p class="text">${description.slice(0, 130)}...</p>
+                <button class="details-btn">See Details</button>
             </div>
         `
     }
 }
 
+$(".create-job-btn").addEventListener("click", (e) => {
+    $("#modal-container").style.display = "block"
+})
+
+$("#btn-close-modal").addEventListener("click", () => {
+    $("#modal-container").style.display = "none"
+})
+
+window.addEventListener("click", (e) => {
+    if (e.target == $("#modal-container")) {
+        $("#modal-container").style.display = "none"
+    }
+})
+
+window.addEventListener("keydown", (e) => {
+    if (e.key === "Escape") {
+        $("#modal-container").style.display = "none"
+    }
+})
